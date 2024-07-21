96362e9f0a58d1ca7d0bcc4f72f0052ab734f734
Aman Singh <amansinghmarch16@gmail.com>
2024-07-21 15:20:43 +0530

    add suspense wrapper where useSearchParams is used

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -14,6 +14,7 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination"
 import { Button } from '@/components/ui/button'
+import { Suspense } from 'react'
 
 interface Categories {
   id: number;
@@ -87,94 +88,96 @@ const Category = () => {
 
 
   return (
-    <main>
-      <header>
-        <Navbar />
-      </header>
-      <section>
-        <div className='flex flex-row my-2'>
-          <div className='w-4/5 md:w-11/12'>
-            <h1 className='text-xl font-semibold px-4'>
-              Hi {user && user.name} !!
-            </h1>
+    <Suspense>
+      <main>
+        <header>
+          <Navbar />
+        </header>
+        <section>
+          <div className='flex flex-row my-2'>
+            <div className='w-4/5 md:w-11/12'>
+              <h1 className='text-xl font-semibold px-4'>
+                Hi {user && user.name} !!
+              </h1>
+            </div>
+            <div>
+              <Button onClick={() => {
+                localStorage.removeItem("token");
+                router.push("/")
+              }}>Logout</Button>
+            </div>
+
           </div>
+        </section>
+        <section>
           <div>
-            <Button onClick={() => {
-              localStorage.removeItem("token");
-              router.push("/")
-            }}>Logout</Button>
-          </div>
-
-        </div>
-      </section>
-      <section>
-        <div>
-          <div className='flex justify-center items-center'>
-            <Card className='md:w-2/6'>
-              <CardHeader>
-                <CardTitle>
-                  Please Mark your interest!
-                </CardTitle>
-                <CardDescription>
-                  We will keep you notified
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div>
+            <div className='flex justify-center items-center'>
+              <Card className='md:w-2/6'>
+                <CardHeader>
+                  <CardTitle>
+                    Please Mark your interest!
+                  </CardTitle>
+                  <CardDescription>
+                    We will keep you notified
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
                   <div>
-                    <p>
-                      My saved interests!
-                    </p>
-                  </div>
-                  <div>
-                    {categories?.map((el) => {
-                      return <div>
-                        <div className='flex space-x-3 my-2'>
-                          <input type='checkbox' onChange={(e) => updateCategory(e.target, el.id, el.title)} id={`${el.id}`}
-                            checked={isChecked(el.id)}
-                          />
+                    <div>
+                      <p>
+                        My saved interests!
+                      </p>
+                    </div>
+                    <div>
+                      {categories?.map((el) => {
+                        return <div>
+                          <div className='flex space-x-3 my-2'>
+                            <input type='checkbox' onChange={(e) => updateCategory(e.target, el.id, el.title)} id={`${el.id}`}
+                              checked={isChecked(el.id)}
+                            />
 
-                          <label htmlFor={`${el.id}`}>{el.title}</label>
+                            <label htmlFor={`${el.id}`}>{el.title}</label>
+                          </div>
                         </div>
-                      </div>
-                    })}
-                  </div>
-                  <div>
-                    <Pagination>
-                      <PaginationContent>
-                        <PaginationItem>
-                          <PaginationPrevious href={(pageNo !== "1") ? `/category?page=${parseInt(pageNo!) - 1}` : `#`} />
-                        </PaginationItem>
-                        <PaginationItem>
-                          <PaginationLink href={(parseInt(pageNo!) !== 1) ? `/category?page=${parseInt(pageNo!) - 1}` : `#`}>{parseInt(pageNo!) - 1}</PaginationLink>
-                        </PaginationItem>
-                        <PaginationItem>
-                          <PaginationLink href="#" isActive>
-                            {pageNo}
-                          </PaginationLink>
-                        </PaginationItem>
-                        <PaginationItem>
-                          <PaginationLink href={(parseInt(pageNo!) <= 16) ? `/category?page=${parseInt(pageNo!) + 1}` : `#`}>{parseInt(pageNo!) + 1}</PaginationLink>
-                        </PaginationItem>
-                        <PaginationItem>
-                        </PaginationItem>
-                        <PaginationItem>
-                          <PaginationEllipsis />
-                        </PaginationItem>
-                        <PaginationItem>
-                          <PaginationNext href={(parseInt(pageNo!) <= 16) ? `/category?page=${parseInt(pageNo!) + 1}` : `#`} />
-                        </PaginationItem>
-                      </PaginationContent>
-                    </Pagination>
+                      })}
+                    </div>
+                    <div>
+                      <Pagination>
+                        <PaginationContent>
+                          <PaginationItem>
+                            <PaginationPrevious href={(pageNo !== "1") ? `/category?page=${parseInt(pageNo!) - 1}` : `#`} />
+                          </PaginationItem>
+                          <PaginationItem>
+                            <PaginationLink href={(parseInt(pageNo!) !== 1) ? `/category?page=${parseInt(pageNo!) - 1}` : `#`}>{parseInt(pageNo!) - 1}</PaginationLink>
+                          </PaginationItem>
+                          <PaginationItem>
+                            <PaginationLink href="#" isActive>
+                              {pageNo}
+                            </PaginationLink>
+                          </PaginationItem>
+                          <PaginationItem>
+                            <PaginationLink href={(parseInt(pageNo!) <= 16) ? `/category?page=${parseInt(pageNo!) + 1}` : `#`}>{parseInt(pageNo!) + 1}</PaginationLink>
+                          </PaginationItem>
+                          <PaginationItem>
+                          </PaginationItem>
+                          <PaginationItem>
+                            <PaginationEllipsis />
+                          </PaginationItem>
+                          <PaginationItem>
+                            <PaginationNext href={(parseInt(pageNo!) <= 16) ? `/category?page=${parseInt(pageNo!) + 1}` : `#`} />
+                          </PaginationItem>
+                        </PaginationContent>
+                      </Pagination>
 
+                    </div>
                   </div>
-                </div>
-              </CardContent>
-            </Card>
+                </CardContent>
+              </Card>
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </Suspense>
   )
 }
 
--- app/verifyuser/page.tsx
+++ app/verifyuser/page.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/input-otp"
 import { Button } from '@/components/ui/button'
 import axios from 'axios'
+import { Suspense } from 'react'
 
 
 const VerifyUser = () => {
@@ -18,112 +19,116 @@ const VerifyUser = () => {
     const length = params.get("email")?.length;
     const [otpVal, setOtpVal] = useState("");
     const router = useRouter();
-    const [warning,setWarning] = useState("");
+    const [warning, setWarning] = useState("");
 
     useEffect(() => {
-        
+
     }, [])
-    
-    const ValidateOTP = async()=>{
+
+    const ValidateOTP = async () => {
         console.log(otpVal);
-        await axios.post("/api/verifyOtp",{
-            email:params.get("email"),
-            otp:otpVal
-        }).then(res=>{
+        await axios.post("/api/verifyOtp", {
+            email: params.get("email"),
+            otp: otpVal
+        }).then(res => {
             console.log(res.data)
-            if(res.data.auth){
+            if (res.data.auth) {
                 router.push("/category?page=1")
             }
-            else{
+            else {
                 setWarning("Wrong OTP please try again")
             }
         })
     }
 
 
-    if(length===undefined){
-        return <main>
-            <header>
-                <Navbar/>
-            </header>
-            <section>
-                <div>
-                    Access restricted !!
-                </div>
-                <div>
-                    <Button onClick={()=>router.push("/")}>Go To Home</Button>
-                </div>
-            </section>
-        </main>
+    if (length === undefined) {
+        return <Suspense>
+            <main>
+                <header>
+                    <Navbar />
+                </header>
+                <section>
+                    <div>
+                        Access restricted !!
+                    </div>
+                    <div>
+                        <Button onClick={() => router.push("/")}>Go To Home</Button>
+                    </div>
+                </section>
+            </main>
+        </Suspense>
     }
 
 
     return (
-        <main>
-            <header>
-                <Navbar />
-            </header>
-            <section>
-                <div>
-                    <div className='flex justify-center items-center'>
-                        <Card className='md:w-2/6 mt-8  border border-gray-400'>
-                            <CardHeader className='text-center'>
-                                <CardTitle>
-                                    <h1 className='mb-4 text-2xl font-semibold'>
-                                        Verify your email
-                                    </h1>
-                                </CardTitle>
-                                <CardDescription className='text-black'>
-                                    <p>
-                                        Enter the 8 digit code you received on
-                                    </p>
-                                    <p>
-                                        {params.get("email")?.slice(0, 3)}{"*****"}
-                                        {params.get("email")?.slice(length! - 10,)}
-                                    </p>
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <div className='flex flex-col justify-center items-center'>
-                                    <p className='capitalize my-2'>
-                                        code
-                                    </p>
-                                    <div>
-                                        <InputOTP value={otpVal}
-                                            onChange={(value) => {
-                                                setOtpVal(value)
-                                                setWarning("")
-                                            }} maxLength={8}>
-                                            <InputOTPGroup>
-                                                <InputOTPSlot index={0} />
-                                                <InputOTPSlot index={1} />
-                                                <InputOTPSlot index={2} />
-                                                <InputOTPSlot index={3} />
-                                            </InputOTPGroup>
-                                            <InputOTPSeparator />
-                                            <InputOTPGroup>
-                                                <InputOTPSlot index={4} />
-                                                <InputOTPSlot index={5} />
-                                                <InputOTPSlot index={6} />
-                                                <InputOTPSlot index={7} />
-                                            </InputOTPGroup>
-                                        </InputOTP>
+        <Suspense>
+            <main>
+                <header>
+                    <Navbar />
+                </header>
+                <section>
+                    <div>
+                        <div className='flex justify-center items-center'>
+                            <Card className='md:w-2/6 mt-8  border border-gray-400'>
+                                <CardHeader className='text-center'>
+                                    <CardTitle>
+                                        <h1 className='mb-4 text-2xl font-semibold'>
+                                            Verify your email
+                                        </h1>
+                                    </CardTitle>
+                                    <CardDescription className='text-black'>
+                                        <p>
+                                            Enter the 8 digit code you received on
+                                        </p>
+                                        <p>
+                                            {params.get("email")?.slice(0, 3)}{"*****"}
+                                            {params.get("email")?.slice(length! - 10,)}
+                                        </p>
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <div className='flex flex-col justify-center items-center'>
+                                        <p className='capitalize my-2'>
+                                            code
+                                        </p>
+                                        <div>
+                                            <InputOTP value={otpVal}
+                                                onChange={(value) => {
+                                                    setOtpVal(value)
+                                                    setWarning("")
+                                                }} maxLength={8}>
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={0} />
+                                                    <InputOTPSlot index={1} />
+                                                    <InputOTPSlot index={2} />
+                                                    <InputOTPSlot index={3} />
+                                                </InputOTPGroup>
+                                                <InputOTPSeparator />
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={4} />
+                                                    <InputOTPSlot index={5} />
+                                                    <InputOTPSlot index={6} />
+                                                    <InputOTPSlot index={7} />
+                                                </InputOTPGroup>
+                                            </InputOTP>
+                                        </div>
                                     </div>
-                                </div>
-                                <div>
-                                    <p className='text-red-700 font-semibold text-center'>
-                                        {warning}
-                                    </p>
-                                </div>
-                                <div>
-                                    <Button onClick={ValidateOTP} className='w-full my-6'>Verify</Button>
-                                </div>
-                            </CardContent>
-                        </Card>
+                                    <div>
+                                        <p className='text-red-700 font-semibold text-center'>
+                                            {warning}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <Button onClick={ValidateOTP} className='w-full my-6'>Verify</Button>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </div>
                     </div>
-                </div>
-            </section>
-        </main>
+                </section>
+            </main>
+        </Suspense>
     )
 }
 
